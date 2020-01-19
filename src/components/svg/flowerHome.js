b969cf5d8533381a9fec140b64c572a95bf11f5e
Ale <ale.sacaza98@gmail.com>
2020-01-19 17:06:58 -0600

    fixing warnings

--- src/components/svg/flowerHome.js
+++ src/components/svg/flowerHome.js
@@ -1,8 +1,6 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-function Flower(props) {
-  const [ animation, setAnimation ] = useState(false)
-
+function Flower() {
   return(
     <svg className='flower-svg' >
       <g className='draw-stroke'>
